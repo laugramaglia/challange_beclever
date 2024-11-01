370a36fd331a18797fa6ceea7edad10a7bca6102
Lau <laugramaglia@gmail.com>
2024-11-01 11:58:26 -0300

    feat: create finish auth onboarding page

--- lib/core/config/router/routes.dart
+++ lib/core/config/router/routes.dart
@@ -1,3 +1,4 @@
+import 'package:challange_beclever/features/auth/presentation/ui/pages/finish_register_page.dart';
 import 'package:challange_beclever/features/auth/presentation/ui/pages/lander_page.dart';
 import 'package:challange_beclever/features/auth/presentation/ui/pages/login_page.dart';
 import 'package:challange_beclever/features/auth/presentation/ui/pages/register_page.dart';
@@ -12,7 +13,9 @@ part 'routes.g.dart';
     path: 'lander',
     routes: [
       TypedGoRoute<LoginRoute>(path: 'login'),
-      TypedGoRoute<RegisterRoute>(path: 'register'),
+      TypedGoRoute<RegisterRoute>(path: 'register', routes: [
+        TypedGoRoute<FinishRegisterRoute>(path: 'finish'),
+      ]),
     ],
   ),
 ])
@@ -57,5 +60,25 @@ class LoginRoute extends GoRouteData {
   @override
   Widget build(BuildContext context, GoRouterState state) {
     return const LoginPage();
+  }
+}
+
+@immutable
+class FinishRegisterRoute extends GoRouteData {
+  final bool $extra;
+  const FinishRegisterRoute({this.$extra = false});
+
+  @override
+  CustomTransitionPage<void> buildPage(
+      BuildContext context, GoRouterState state) {
+    return CustomTransitionPage<void>(
+        key: state.pageKey,
+        child: FinishRegisterPage(
+          isSuccess: $extra,
+        ),
+        transitionsBuilder: (BuildContext _, Animation<double> animation,
+            Animation<double> __, Widget child) {
+          return ScaleTransition(scale: animation, child: child);
+        });
   }
 }
