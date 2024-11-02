a264fbe5f2e4fa122d062195d9ce88dc2884af1f
Lau <laugramaglia@gmail.com>
2024-11-02 11:04:42 -0300

    fix: add loader to ui

--- lib/features/auth/presentation/ui/pages/register_sections/new_password_section.dart
+++ lib/features/auth/presentation/ui/pages/register_sections/new_password_section.dart
@@ -1,4 +1,7 @@
+import 'package:challange_beclever/core/config/bloc/service_locator.dart';
 import 'package:challange_beclever/core/config/router/routes.dart';
+import 'package:challange_beclever/core/theme/mixins/loading_overlay_mixin.dart';
+import 'package:challange_beclever/features/auth/presentation/bloc/auth_bloc/authentication_bloc.dart';
 import 'package:challange_beclever/features/auth/presentation/bloc/register_user/register_user_cubit.dart';
 import 'package:challange_beclever/features/auth/presentation/ui/widgets/onboarding_main_section.dart';
 import 'package:challange_beclever/features/auth/presentation/ui/widgets/text_field_widget.dart';
@@ -6,65 +9,106 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NewPasswordSection extends StatelessWidget {
+class NewPasswordSection extends StatefulWidget {
   const NewPasswordSection({super.key});
+
+  @override
+  State<NewPasswordSection> createState() => _NewPasswordSectionState();
+}
+
+class _NewPasswordSectionState extends State<NewPasswordSection>
+    with LoadingOverlayMixin {
+  OverlayEntry? _overlayEntry;
+  void _removeLoadingOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  void _showOverlay() {
+    if (_overlayEntry != null) return;
+    _overlayEntry = createOverlay();
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  @override
+  void dispose() {
+    _removeLoadingOverlay();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final userState = context.watch<RegisterUserCubit>().state;
-    return OnboardingMainSection(
-      titleLabel: 'Ingresa tu nueva contraseña',
-      onPressedFloatingActionButton: !userState.isPasswordMatched
-          ? null
-          : () {
-              const FinishRegisterRoute($extra: true).pushReplacement(context);
+    return BlocListener<AuthenticationBloc, AuthenticationState>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+          _removeLoadingOverlay();
+          const FinishRegisterRoute($extra: true).pushReplacement(context);
+        }
+
+        if (state is AuthLoading) {
+          _showOverlay();
+        }
+
+        if (state is AuthError) {
+          _removeLoadingOverlay();
+        }
+      },
+      child: OnboardingMainSection(
+        titleLabel: 'Ingresa tu nueva contraseña',
+        onPressedFloatingActionButton: !userState.isPasswordMatched
+            ? null
+            : () {
+                sl<AuthenticationBloc>()
+                    .add(CreatePassword(password: userState.password));
+              },
+        children: [
+          TextFieldWidget(
+            keyboardType: TextInputType.visiblePassword,
+            validators: const [
+              TextInputValidators.required,
+              TextInputValidators.password
+            ],
+            inputFormatters: [
+              FilteringTextInputFormatter.singleLineFormatter,
+            ],
+            labelText: 'Contraseña',
+            onChange: (value, error) {
+              if (error == null && value.isNotEmpty) {
+                context.read<RegisterUserCubit>().updatePassword(value);
+              } else if (userState.password.isNotEmpty) {
+                context.read<RegisterUserCubit>().updatePassword('');
+              }
             },
-      children: [
-        TextFieldWidget(
-          keyboardType: TextInputType.visiblePassword,
-          validators: const [
-            TextInputValidators.required,
-            TextInputValidators.password
-          ],
-          inputFormatters: [
-            FilteringTextInputFormatter.singleLineFormatter,
-          ],
-          labelText: 'Contraseña',
-          onChange: (value, error) {
-            if (error == null && value.isNotEmpty) {
-              context.read<RegisterUserCubit>().updatePassword(value);
-            } else if (userState.password.isNotEmpty) {
-              context.read<RegisterUserCubit>().updatePassword('');
-            }
-          },
-        ),
-        const SizedBox(height: 16),
-        TextFieldWidget(
-          keyboardType: TextInputType.visiblePassword,
-          validators: const [
-            TextInputValidators.required,
-            TextInputValidators.custom,
-            TextInputValidators.password,
-          ],
-          customValidator: (value) {
-            if (value != userState.password) {
-              return 'La contraseña no coincide';
-            }
-            return null;
-          },
-          inputFormatters: [
-            FilteringTextInputFormatter.singleLineFormatter,
-          ],
-          labelText: 'Repite la contraseña',
-          onChange: (value, error) {
-            if (error == null && value.isNotEmpty) {
-              context.read<RegisterUserCubit>().updateConfirmPassword(value);
-            } else if (userState.confirmPassword.isNotEmpty) {
-              context.read<RegisterUserCubit>().updateConfirmPassword('');
-            }
-          },
-        ),
-      ],
+          ),
+          const SizedBox(height: 16),
+          TextFieldWidget(
+            keyboardType: TextInputType.visiblePassword,
+            validators: const [
+              TextInputValidators.required,
+              TextInputValidators.custom,
+              TextInputValidators.password,
+            ],
+            customValidator: (value) {
+              if (value != userState.password) {
+                return 'La contraseña no coincide';
+              }
+              return null;
+            },
+            inputFormatters: [
+              FilteringTextInputFormatter.singleLineFormatter,
+            ],
+            labelText: 'Repite la contraseña',
+            onChange: (value, error) {
+              if (error == null && value.isNotEmpty) {
+                context.read<RegisterUserCubit>().updateConfirmPassword(value);
+              } else if (userState.confirmPassword.isNotEmpty) {
+                context.read<RegisterUserCubit>().updateConfirmPassword('');
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
